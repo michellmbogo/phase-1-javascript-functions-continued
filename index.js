809e5f52a319c3a732de9e5f4a364c8b9052ea92
Michell Wambui Mbogo <mishie254@gmail.com>
2024-12-02 18:21:54 +0600

    function extension lab

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // code your solution here
+// saturdayFun function
 function saturdayFun(activity = "roller-skate") {
-    console.log(`This Saturday I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork function
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+// wrapAdjective function
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
 }
